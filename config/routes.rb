c2422868441281c1f00ab006f42dbb20c826fd38
Elizabeth Hagearty <elizabeth@glassymedia.com>
2016-06-19 20:48:32 -0700

    adding a new test route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get 'date' => 'welcome#date', as: :date
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
